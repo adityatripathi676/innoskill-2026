497354a8c959ea98cbd1f43c28eecffbd6151e8b
Aditya Tripathi <adityatripathi676@gmail.com>
2026-03-14 11:25:31 +0530

    Update form-header component

--- innoskill-frontend/src/components/form-header.tsx
+++ innoskill-frontend/src/components/form-header.tsx
@@ -9,7 +9,7 @@ export default function FormHeader(){
         <div className="flex flex-col justify-center items-center">
         <Image src={mrlogo} alt="MRIIRS Logo" width={200} className="select-none w-full max-w-[200px] h-auto opacity-90" />
         <div className="text-center my-3 max-w-4xl">
-            <h1 className="font-semibold text-sm text-slate-500 tracking-wide">8th Edition</h1>
+            <h1 className="font-semibold text-sm text-slate-500 tracking-wide">9th Edition</h1>
             <span className={roboto.className}>
                 <h1 className="font-extrabold text-2xl md:text-3xl bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 bg-clip-text text-transparent uppercase">INNOSKILLS 2026</h1>
             </span>
